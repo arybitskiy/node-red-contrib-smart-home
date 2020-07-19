18ec4f6ae2562764f7926b3a1707d43669a675f5
arybitskiy <alekso.php@gmail.com>
2020-07-19 23:12:56 +0200

    0.0.5

---
+++ publish.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const cli = require('cli');
+
+const packageJSONFilePath = path.resolve(__dirname, './package.json');
+
+const readFile = () => JSON.parse(fs.readFileSync(packageJSONFilePath));
+
+const writeFile = contents => fs.writeFileSync(packageJSONFilePath, JSON.stringify(contents, null, 4));
+
+const config = cli.parse({
+  type: ['t', 'Before/after publish (pre|post)', ['pre', 'post'], null],
+});
+
+switch (config.type) {
+  case 'pre':
+    const preJSON = readFile();
+    delete preJSON.private;
+    writeFile(preJSON);
+    break;
+  case 'post':
+    const postJSON = readFile();
+    postJSON.private = true;
+    writeFile(postJSON);
+    break;
+}

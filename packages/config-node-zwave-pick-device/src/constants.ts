66d7d43b44e05f83df0d6d572bacc9f5b4487d8d
arybitskiy <alekso.php@gmail.com>
2020-07-22 21:06:03 +0200

    Fix

--- packages/config-node-zwave-pick-device/src/constants.ts
+++ packages/config-node-zwave-pick-device/src/constants.ts
@@ -3,4 +3,3 @@ export const VALUES_SET_EVENT = 'values_set';
 export const CONTEXT = 'context';
 export const READ_CONTEXT_ENDPOINT = 'config-node-zwave-pick-device-backend-read-context';
 export const MQTT_PREFIX = 'openzwave2mqtt';
-export const NODE_PREFIX = 'nodeID_';

99219eaeaa1841c5d1a38ceb6cf855c6e661bd1c
arybitskiy <alekso.php@gmail.com>
2020-07-30 03:54:21 +0200

    Fix

--- packages/config-node-zwave-pick-device/src/constants.ts
+++ packages/config-node-zwave-pick-device/src/constants.ts
@@ -5,5 +5,5 @@ export const NODE_TYPE = 'config-node-zwave-pick-device';
 export const READ_CONTEXT_ENDPOINT = `${NODE_TYPE}-backend-read-context`;
 export const MQTT_PREFIX = 'openzwave2mqtt';
 export const READ_ALL_NODES = `${NODE_TYPE}-backend-read-all-nodes`;
-export const WEBSOCKET_PATH = `/${NODE_TYPE}-websocket`;
+export const WEBSOCKET_PATH = `${NODE_TYPE}-websocket`;
 export const WEBSOCKET_MESSAGE_EVENT = 'websocketMessage';

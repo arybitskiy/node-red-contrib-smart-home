4c444d4014796455c8e83d9f34aeaccb1589e152
arybitskiy <alekso.php@gmail.com>
2020-07-30 03:55:48 +0200

    Fixes

--- packages/node-planner/src/ui/WebSocketListener.ts
+++ packages/node-planner/src/ui/WebSocketListener.ts
@@ -8,7 +8,7 @@ import { ActionTypes } from './types';
 export const WebSocketListener = () => {
   const dispatch = useDispatch();
   const { lastJsonMessage } = useWebsocket(
-    location.origin.replace(/^http/, 'ws') + location.pathname + WEBSOCKET_PATH,
+    location.origin.replace(/^http/, 'ws') + location.pathname.replace(/\/$/, '') + WEBSOCKET_PATH,
     {
       share: true,
       shouldReconnect: () => true,

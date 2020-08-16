548ba28ecdc22e48d5c85d9ba565289cd79bd2d4
arybitskiy <alekso.php@gmail.com>
2020-08-16 20:47:40 +0200

    Fix

--- packages/node-presence-detection/src/constants.ts
+++ packages/node-presence-detection/src/constants.ts
@@ -2,7 +2,7 @@ export const NODES_NORMALIZED = 'NODES_NORMALIZED';
 export const NODE_NORMALIZED = 'NODE_NORMALIZED';
 export const ZONE_PROBABILITY = 'ZONE_PROBABILITY';
 
-export const LOCK_TIMEOUT = 15 * 1000;
+export const LOCK_TIMEOUT = 20 * 1000;
 export const MOTION_TIMEOUT = 0;
 export const SWITCH_TO_INACTIVE_AFTER = 2 * 60 * 1000;
 export const SWITCH_TO_INACTIVE_AFTER_TICK = 5 * 1000;

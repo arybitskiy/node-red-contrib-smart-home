daf90ad60f32662bb998998235a1bb79ff602a75
arybitskiy <alekso.php@gmail.com>
2020-08-26 00:50:08 +0200

    Fix

--- packages/config-node-zwave-pick-device/src/devices/fibaro/utils.ts
+++ packages/config-node-zwave-pick-device/src/devices/fibaro/utils.ts
@@ -3,11 +3,13 @@ export const startLightManager = (turnOnOrOff: (nextState: boolean) => void, del
 
   return [
     (nextState: boolean) => {
+      console.log('nextState: ', nextState);
       clearTimeout(timeout);
       if (nextState) {
         console.log('turn on');
         turnOnOrOff(nextState);
       } else {
+        console.log('delayInSeconds * 1000: ', delayInSeconds * 1000);
         timeout = setTimeout(() => {
           console.log('turn off');
           turnOnOrOff(nextState);
